a61fd50a3efa8a32733f8bfae7aa819c5341e343
Wen Feitan <181696474+WenfeiTan@users.noreply.github.com>
2025-07-17 15:24:00 +0800

    1st commit from fea1

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ app.use(express.static(publicDirectoryPath));
 
 io.on("connection", socket => {
   console.log("New WebSocket connection");
+  console.log("This is new console.log from feature1 by Alexis Tan")
 
   socket.on("join", (options, callback) => {
     const { error, user } = addUser({ id: socket.id, ...options });
